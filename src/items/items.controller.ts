f06a014cd5eb61b0a989cdf7feb5a2c26988ea97
Aditi <adtsolanki22@gmail.com>
2022-12-19 19:53:17 +0530

    Added  item request

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller,Body, Get, Post } from '@nestjs/common';
+import { CreateItemDTO } from './dto/create-item.dto';
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    @Get()
+    findAll(): string {
+        return "Get all items";
+    }
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDTO): string{
+        return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`; 
+    }
+}
