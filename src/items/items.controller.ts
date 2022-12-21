94c3928221cca83419bc7206a13799b05f30d726
Aditi <adtsolanki22@gmail.com>
2022-12-21 12:23:49 +0530

    Added Methods into Controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,14 +1,36 @@
-import { Controller,Body, Get, Post } from '@nestjs/common';
+import { Controller,Body, Get, Post, Req, Res, Param, Put, Delete} from '@nestjs/common';
 import { CreateItemDTO } from './dto/create-item.dto';
+import { Request, Response } from 'express';
 @Controller('items')
 export class ItemsController {
     @Get()
-    findAll(): string {
-        return "Get all items";
+    findAll(@Req() req: Request, @Res() res: Response): Response {
+        console.log(req.url);
+        return res.send("Hello World!!!");
     }
+
+
+    @Get(":id")
+    findOne(@Param() param) : string{
+        return `item ${param.id}`
+    }
+    // findOne(@Param('id') id) : string{
+    //     return `item ${id}`
+    // }
 
     @Post()
     create(@Body() createItemDto: CreateItemDTO): string{
         return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`; 
     }
+
+    @Delete(':id')
+    delete(@Param('id') id) {
+        return `Delete ${id}`
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto: CreateItemDTO, @Param('id') id) : string {
+        return `Update ${id} - Name: ${updateItemDto.name}`
+    }
+
 }
